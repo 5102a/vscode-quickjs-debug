7cf308274a33451b998c929e5f76c603487b0f81
Koushik Dutta <koushd@gmail.com>
2019-11-06 17:31:33 -0800

    add test file

---
+++ src/tests/data/test.js
@@ -0,0 +1,34 @@
+const gg = 9;
+function foo (t) {
+    var a = 55;
+    var b = 33;
+    var c = {
+        d: true,
+        e: 'hello',
+        f: 34.55,
+    };
+
+    function noob() {
+        console.log('f;asdsad`')
+        console.log(a);
+        console.log(t);
+        console.log('supsups')
+        console.log('ubgasdsad')
+    }
+    noob();
+}
+
+function bar() {
+    foo(3);
+}
+
+class Blub {
+    peeps = 3;
+    jib() {
+        console.log(this);
+        bar();
+    }
+}
+
+var blub = new Blub();
+blub.jib();
